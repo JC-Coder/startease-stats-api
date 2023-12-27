7d7180f821e3c661789b9c180a23327fae1a2d49
JC-Coder <josephchimezie2003@gmail.com>
2023-12-27 12:56:49 +0100

    update

--- src/common/utils/logger.js
+++ src/common/utils/logger.js
@@ -1,22 +1,34 @@
-import winston from "winston";
+import winston from 'winston';
+import fs from 'fs';
+import path from 'path';
+
+const logDir = path.join(process.cwd(), 'logs');
+
+// Ensure logs directory exists
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
 
 export const logger = winston.createLogger({
-  level: "info",
+  level: 'info',
   format: winston.format.combine(
-    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
     winston.format.printf(({ level, message, timestamp }) => {
       const logEntry = `${timestamp} ${level}: ${message}`;
-      return logEntry.replace(/\u001b\[0m/g, "");
-    }),
+      return logEntry.replace(/\u001b\[0m/g, '');
+    })
   ),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: "logs/info.log", level: "info" }),
-  ],
+    new winston.transports.File({
+      filename: path.join(logDir, 'info.log'),
+      level: 'info'
+    })
+  ]
 });
 
 export const stream = {
   write: (message) => {
     logger.info(message.trim());
-  },
+  }
 };
